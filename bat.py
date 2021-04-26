67f47d15f0eb3f66f46b5977fbe04105bd54ab26
wojsikora <63751217+wojsikora@users.noreply.github.com>
2021-04-26 11:02:26 +0200

    Update bat.py

--- bat.py
+++ bat.py
@@ -4,8 +4,13 @@ import pygame
 
 class Bat(Enemy):
 
-    def __init__(self):
-        super().__init__(game, 25)
-        self.p_attack = 10
+    def __init__(self, game, health, p_attack):
+        super().__init__(game, health, p_attack)
+        #self.p_attack = 10
+        #self.health = 5
+
+    def bat_attack(self, player):
+        if int(self.x) == int(player.x) and int(self.y) == int(player.y):
+            player.health-=5
 
 
